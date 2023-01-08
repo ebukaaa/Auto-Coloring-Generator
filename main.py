6f1c44076bc07d028e500d9a330035f4f84e44df
ebukaa <>
2023-01-08 14:12:06 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+#1. Create a greeting for your program.
+print ("Welcome To The AutoColouring Generator")
+#3. Ask the user for their favourite car.
+car = input ("What is the name of your favourite car? \n")
+#2. Ask the user for their favourite colour.
+colour = input ("What is your favourite colour? \n")
+#4. Combine the name of their favourite colour and car and show them their Car name and colour.
+print ("Your car colour and name is " + colour + " " + car)
+#Another step of combining;
+#car_colour = car + " " + colour
+#1
+#print (f"Your car name and colour is {car_colour}")
+#2
+#I learned this particular step from Dr. Charles.
+#print ("Your car name and colour is " + car, colour)
+#3
+#print ("Your car name and colour is " + car_colour)
